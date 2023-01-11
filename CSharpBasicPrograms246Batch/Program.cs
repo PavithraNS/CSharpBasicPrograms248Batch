c8d567c1a15069e85af18da22c97b7bec06d2b28
PavithraNS <nspavi1991@gmail.com>
2023-01-11 09:53:57 +0530

    [Pavithra] Adding CSharp Basics Project

---
+++ CSharpBasicPrograms246Batch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasicPrograms246Batch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Csharp basic programs");
+            Console.ReadLine();
+        }
+    }
+}

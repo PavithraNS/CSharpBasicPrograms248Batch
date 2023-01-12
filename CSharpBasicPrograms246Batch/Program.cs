7fca5cbcce1dd116c16396bf73d17457a6b85f26
PavithraNS <nspavi1991@gmail.com>
2023-01-12 10:09:28 +0530

    [Pavithra] Adding Addition functionality

--- CSharpBasicPrograms246Batch/Program.cs
+++ CSharpBasicPrograms246Batch/Program.cs
@@ -1,16 +1,25 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpBasicPrograms246Batch
+namespace CSharpBasicPrograms248Batch
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Csharp basic programs");
+
+            Console.WriteLine("Please enter first number");
+            int first=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            Calculator calculator = new Calculator(first,second);
+            calculator.Addition();
             Console.ReadLine();
         }
     }
